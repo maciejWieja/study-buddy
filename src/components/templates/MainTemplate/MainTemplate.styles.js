4db8b4052fef1ed639b2a6187bdc817234f130a4
maciejWieja <wiejamaciej@gmail.com>
2023-05-21 21:12:19 +0200

    news-final

--- src/components/templates/MainTemplate/MainTemplate.styles.js
+++ src/components/templates/MainTemplate/MainTemplate.styles.js
@@ -2,11 +2,13 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 150px 1fr;
+  grid-template-columns: 150px 1fr 0.75fr;
+  grid-template-rows: 90px 1fr;
   background-color: ${({ theme }) => theme.colors.lightGrey};
   margin: 0;
   padding: 0;
   width: 100%;
   height: 100vh;
   overflow-x: scroll;
+  overflow-y: hidden;
 `;
