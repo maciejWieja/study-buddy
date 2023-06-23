684bc62b1f236548587691b4b1fde3bf850ade23
maciejWieja <wiejamaciej@gmail.com>
2023-06-23 21:07:02 +0200

    auth-final

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { theme } from 'assets/styles/theme';
@@ -6,20 +6,86 @@ import { Wrapper } from './Root.styles';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
 import Dashboard from './Dashboard';
+import FormField from 'components/moleculs/FormField/FormField';
+import { Button } from 'components/atoms/Button/Button';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+
+const AuthenticatedApp = () => {
+  return (
+    <MainTemplate>
+      <Wrapper>
+        <Routes>
+          <Route exact path="/" element={<Navigate to="/group" />} />
+          <Route path="/group/:id?" element={<Dashboard />} />
+        </Routes>
+      </Wrapper>
+    </MainTemplate>
+  );
+};
+
+const UnAuthenticatedApp = ({ handleSignIn, loginError }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <form
+      onSubmit={handleSubmit(handleSignIn)}
+      style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}
+    >
+      <FormField label="login" name="login" id="login" {...register('login', { required: true })} />
+      {errors.login && <span>Login is required</span>}
+      <FormField label="password" name="password" id="password" type="password" {...register('password', { required: true })} />
+      {errors.password && <span>Password is required</span>}
+      <Button type="submit">Sign in</Button>
+      {loginError && <span>{loginError}</span>}
+    </form>
+  );
+};
 
 const Root = () => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    (async () => {
+      if (token) {
+        try {
+          const response = await axios.get('/me', {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          });
+          setUser(response.data);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    })();
+  }, []);
+
+  const handleSignIn = async ({ login, password }) => {
+    try {
+      const response = await axios.post('/login', {
+        login,
+        password,
+      });
+      setUser(response.data);
+      localStorage.setItem('token', response.data.token);
+    } catch (e) {
+      setError('Please provide valid user data');
+    }
+  };
+
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <MainTemplate>
-          <Wrapper>
-            <Routes>
-              <Route exact path="/" element={<Navigate to="/group" />} />
-              <Route path="/group/:id?" element={<Dashboard />} />
-            </Routes>
-          </Wrapper>
-        </MainTemplate>
+        {user ? <AuthenticatedApp /> : <UnAuthenticatedApp loginError={error} handleSignIn={handleSignIn} />}
       </ThemeProvider>
     </Router>
   );
