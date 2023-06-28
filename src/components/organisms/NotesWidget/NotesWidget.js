9dd4264383ab5e286e49bface3737317f3eb284e
maciejWieja <wiejamaciej@gmail.com>
2023-06-28 21:55:35 +0200

    redux-toolkit-query-final

--- src/components/organisms/NotesWidget/NotesWidget.js
+++ src/components/organisms/NotesWidget/NotesWidget.js
@@ -1,10 +1,10 @@
 import Note from 'components/moleculs/Note/Note';
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
 import { NotesWrapper, WidgetHandler, Wrapper } from './NotesWidget.styles';
+import { useGetNotesQuery } from 'store';
 
 const NotesWidget = () => {
-  const notes = useSelector((state) => state.notes);
+  const { data, isLoading } = useGetNotesQuery();
   const [isOpen, setIsOpen] = useState(false);
 
   const handleToggleWidget = () => setIsOpen((prevState) => !prevState);
@@ -12,13 +12,17 @@ const NotesWidget = () => {
   return (
     <Wrapper isOpen={isOpen}>
       <WidgetHandler onClick={handleToggleWidget}>notes</WidgetHandler>
-      <NotesWrapper>
-        {notes.length ? (
-          notes.map(({ title, content, id }) => <Note key={id} title={title} content={content} id={id} />)
-        ) : (
-          <p>Create your first note</p>
-        )}
-      </NotesWrapper>
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <NotesWrapper>
+          {data.notes.length ? (
+            data.notes.map(({ title, content, id }) => <Note key={id} title={title} content={content} id={id} />)
+          ) : (
+            <p>Create your first note</p>
+          )}
+        </NotesWrapper>
+      )}
     </Wrapper>
   );
 };
