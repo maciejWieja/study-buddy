940d22976c816d79a0a8a2093a15162bcf4ef34c
maciejWieja <wiejamaciej@gmail.com>
2023-06-30 16:53:14 +0200

    summary-refactor-final

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -2,41 +2,49 @@ import React, { useEffect, useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import StudentsList from 'components/organisms/StudentsList/StudentsList';
-import { useStudents } from 'hooks/useStudents';
 import { GroupWrapper, TitleWrapper, Wrapper } from 'views/Dashboard.styles';
 import { Title } from 'components/atoms/Title/Title';
 import useModal from 'components/organisms/Modal/useModal';
 import StudentDetails from 'components/moleculs/StudentDetails/StudentDetails';
 import Modal from 'components/organisms/Modal/Modal';
+import { useGetGroupsQuery } from 'store';
+import { useError } from 'hooks/useError';
+import { useGetStudentByIdMutation } from 'store';
 
 const Dashboard = () => {
-  const [groups, setGroups] = useState([]);
   const [currentStudent, setCurrentStudent] = useState([]);
-  const { getGroups, getStudentById } = useStudents();
   const { id } = useParams();
   const { isOpen, handleOpenModal, handleCloseModal } = useModal();
-
-  useEffect(() => {
-    (async () => {
-      const groups = await getGroups();
-      setGroups(groups);
-    })();
-  }, [getGroups]);
+  const { data, isLoading } = useGetGroupsQuery();
+  const { dispatchError } = useError();
+  const [getStudentById] = useGetStudentByIdMutation();
 
   const handleOpenStudentsDetails = async (id) => {
-    const student = await getStudentById(id);
-    setCurrentStudent(student);
+    const { data } = await getStudentById(id);
+    setCurrentStudent(data.students);
     handleOpenModal();
   };
 
-  if (!id && groups.length > 0) return <Navigate to={`/group/${groups[0].id}`} />;
+  useEffect(() => {
+    if (data && data.groups.length === 0) dispatchError('There are no groups');
+  }, [data, dispatchError]);
+
+  if (isLoading) {
+    return (
+      <Wrapper>
+        <Title>Loading...</Title>
+      </Wrapper>
+    );
+  }
+
+  if (!id && data.groups.length > 0) return <Navigate to={`/group/${data.groups[0].id}`} />;
 
   return (
     <Wrapper>
       <TitleWrapper>
         <Title as="h2">Group {id}</Title>
         <nav>
-          {groups.map(({ id }) => (
+          {data.groups.map(({ id }) => (
             <Link key={id} to={`/group/${id}`}>
               {id}
             </Link>
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,51 +1,9 @@
 import React from 'react';
-import { Wrapper } from './Root.styles';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
-import Dashboard from './Dashboard';
-import FormField from 'components/moleculs/FormField/FormField';
-import { Button } from 'components/atoms/Button/Button';
-import { useForm } from 'react-hook-form';
 import { useAuth } from 'hooks/useAuth';
 import ErrorMessage from 'components/moleculs/ErrorMessage/ErrorMessage';
 import { useError } from 'hooks/useError';
-import Notes from './Notes';
-
-const AuthenticatedApp = () => {
-  return (
-    <MainTemplate>
-      <Wrapper>
-        <Routes>
-          <Route exact path="/" element={<Navigate to="/group" />} />
-          <Route path="/group/:id?" element={<Dashboard />} />
-          <Route path="/notes" element={<Notes />} />
-        </Routes>
-      </Wrapper>
-    </MainTemplate>
-  );
-};
-
-const UnAuthenticatedApp = () => {
-  const auth = useAuth();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  return (
-    <form
-      onSubmit={handleSubmit(auth.signIn)}
-      style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}
-    >
-      <FormField label="login" name="login" id="login" {...register('login', { required: true })} />
-      {errors.login && <span>Login is required</span>}
-      <FormField label="password" name="password" id="password" type="password" {...register('password', { required: true })} />
-      {errors.password && <span>Password is required</span>}
-      <Button type="submit">Sign in</Button>
-    </form>
-  );
-};
+import AuthenticatedApp from './AuthenticatedApp';
+import UnAuthenticatedApp from './UnAuthenticatedApp';
 
 const Root = () => {
   const auth = useAuth();
